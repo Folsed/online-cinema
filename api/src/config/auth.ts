223b56b20561d5ae62abe9c543abdb437c0f55f8
Kyrylo <dexter37814@gmail.com>
2025-06-21 15:56:23 +0300

    fix: change models fields for better-auth config

--- api/src/config/auth.ts
+++ api/src/config/auth.ts
@@ -17,6 +17,12 @@ const auth = betterAuth({
         sendResetPassword: sendResetPasswordEmail,
     },
     user: {
+        modelName: 'users',
+        fields: {
+            emailVerified: 'email_verified',
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        },
         additionalFields: {
             acceptTerms: {
                 fieldName: 'accept_terms',
@@ -24,6 +30,40 @@ const auth = betterAuth({
                 required: true,
                 defaultValue: false,
             },
+        },
+    },
+    session: {
+        modelName: 'sessions',
+        fields: {
+            userId: 'user_id',
+            expiresAt: 'expires_at',
+            ipAddress: 'ip_address',
+            userAgent: 'user_agent',
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        },
+    },
+    account: {
+        modelName: 'accounts',
+        fields: {
+            userId: 'user_id',
+            accountId: 'account_id',
+            providerId: 'provider_id',
+            accessToken: 'access_token',
+            refreshToken: 'refresh_token',
+            accessTokenExpiresAt: 'access_token_expires_at',
+            refreshTokenExpiresAt: 'refresh_token_expires_at',
+            idToken: 'id_token',
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        },
+    },
+    verification: {
+        modelName: 'verifications',
+        fields: {
+            expiresAt: 'expires_at',
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
         },
     },
 });
