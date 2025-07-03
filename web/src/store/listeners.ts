1c360be9e98a0170c4200674237cf9464628c3f4
Kyrylo <kyrylo.kolhanov@computools.com>
2025-07-03 13:03:44 +0300

    fix: caching of user settings and watchlist after logout

--- web/src/store/listeners.ts
+++ web/src/store/listeners.ts
@@ -2,6 +2,7 @@ import { createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit'
 import { authApiSlice } from '@/store/features/auth/authApiSlice'
 import { watchlistApiSlice } from '@/store/features/user/watchlistApiSlice'
 import { settingsApiSlice } from '@/store/features/user/settingsApiSlice'
+import { watchlistSlice } from '@/store/features/user/watchlistSlice'
 
 export const listenerMiddleware = createListenerMiddleware()
 
@@ -11,7 +12,15 @@ listenerMiddleware.startListening({
         authApiSlice.endpoints.register.matchFulfilled
     ),
     effect: async (action, listenerApi) => {
-        listenerApi.dispatch(settingsApiSlice.endpoints.getUserSettings.initiate(undefined))
-        listenerApi.dispatch(watchlistApiSlice.endpoints.getWatchlist.initiate(undefined))
+        listenerApi.dispatch(settingsApiSlice.endpoints.getUserSettings.initiate())
+        listenerApi.dispatch(watchlistApiSlice.endpoints.getWatchlist.initiate())
+    },
+})
+
+listenerMiddleware.startListening({
+    matcher: isAnyOf(authApiSlice.endpoints.logout.matchFulfilled),
+    effect: async (action, listenerApi) => {
+        listenerApi.dispatch(settingsApiSlice.util.resetApiState())
+        listenerApi.dispatch(watchlistApiSlice.util.resetApiState())
     },
 })
